8bf20a961f5c0b99b1a49dd7c9b2a7349ff6a37a
hitesh <hitesh@oliveglobal.com>
2021-09-11 23:47:49 +0530

    first commit

---
+++ array/array.go
@@ -0,0 +1,15 @@
+package array
+
+import "fmt"
+
+func PrintArray() {
+
+	var a [3]int
+
+	a[2] = 10
+
+	fmt.Println("The value of a is", a)
+
+	//https://play.golang.org/p/zOrX89Fnywj
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"example.com/hello/array"
+	"example.com/hello/variable"
+)
+
+func main() {
+	array.PrintArray()
+	variable.Varriable()
+}
